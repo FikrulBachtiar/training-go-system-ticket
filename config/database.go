34349fc6c139b9c90cc7cb84d93ca822e71d0dae
MFikrulBachtiar <mfikrulb@gmail.com>
2023-03-07 19:46:54 +0700

    [MOD] - Change func name - Create test ping to DB

--- config/database.go
+++ config/database.go
@@ -8,7 +8,7 @@ import (
 )
 
 
-func Hello() *sql.DB {
+func ConnectionDB() *sql.DB {
 	db, err := sql.Open("postgres", `host=localhost port=5432 user=postgres password=postgres dbname=DB_KA_MAKASSAR sslmode=disable`)
 	if err != nil {
 		panic(err)
@@ -18,7 +18,11 @@ func Hello() *sql.DB {
 	db.SetMaxOpenConns(100)
 	db.SetConnMaxIdleTime(5 * time.Minute)
 	db.SetConnMaxLifetime(60 * time.Minute)
-	// defer db.Close()
-	// fmt.Printf("SUCCESS CONNECT")
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 	return db
 }
